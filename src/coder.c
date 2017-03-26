009ec59a0b5ea1b2dbd46bcfb5a714a410e8ed6e
hegemonies <hegemonies@yandex.ru>
2017-03-26 16:36:37 +0700

    Finish func "encode" in coder.c

--- src/coder.c
+++ src/coder.c
@@ -11,19 +11,27 @@ int encode(uint32_t code_point, CodeUnits *code_units);
 	if (count <= 7) {
 		code_units->code[0] = code_point;
 		code_units->lenght = 1;
+		return 0;
 	} else if (count <= 11) {
-		code_units->code[0] = (code_point & 0x3f) | 0x80;
-		code_units->code[1] = (code_point >> 6) | 0xC0;
+		code_units->code[0] = (code_point >> 6) | 0xC0;
+		code_units->code[1] = (code_point & 0x3F) | 0x80;
 		code_units->lenght = 2;
+		return 0;
 	} else if (count <= 16) {
-		code_units->code[0] = (code_point & 0x3f) | 0x80;
+		code_units->code[0] = (code_point >> 12) | 0xE0;
 		code_units->code[1] = ((code_point & 0xFC0) >> 6) | 0x80;
-		code_units->code[2] = code_point >> 12;
+		code_units->code[2] = (code_point & 0x3F) | 0x80;
 		code_units->lenght = 3;
-	} elese if (count <= 21) {
-		code_units->code[0] = code_point & ;
+		return 0;
+	} else if (count <= 21) {
+		code_units->code[0] = (code_point >> 18) | 0xF0;
+		code_units->code[1] = (code_point & 0x3F000) >> 12;
+		code_units->code[2] = (code_point & 0xFC0) >> 6;
+		code_units->code[3] = code_point & 0x3F;
 		code_units->lenght = 4;
+		return 0;
 	}
+	return -1;
 }
 /*
 uint32_t decode(const CodeUnit *code_unit)
